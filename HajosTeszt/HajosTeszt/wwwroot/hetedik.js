158e86b028b582b1ad75e7dd769b035920057456
corvinus-jxt728 <ferenc.barath@stud.uni-corvinus.hu>
2021-03-25 13:43:07 +0100

    Igyekeztem, de nem megy

--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -1,5 +1,6 @@
 var kérdések;
-
+window.onload = letöltés();
+window.onload = kérdésMegjelenítés(0);
 function letöltés() {
     fetch('/questions.json')
         .then(response => response.json())
@@ -11,4 +12,14 @@ function letöltésBefejezõdött(d) {
     console.log("Sikeres letöltés")
     console.log(d)
     kérdések = d;
+}
+
+function kérdésMegjelenítés(kérdés) {
+    document.getElementById("kérdés_szöveg")
+
+    kérdés[i].questionText
+    let elem = document.createElement("div")
+    elem.innerHTML = kérdés[i].questionText
+    ide.appendChild(elem)
+
 }
